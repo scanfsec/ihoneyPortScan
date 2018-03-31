2035ad87f95767f6251ab9e2f120ed30b0c8f1aa
ihoney <102505481@qq.com>
2018-03-31 17:05:00 +0800

    project start

---
+++ ihoneyPortScan.py
@@ -0,0 +1,127 @@
+# coding=utf-8
+from queue import Queue
+from threading import Thread
+from telnetlib import Telnet
+from base64 import b64decode
+from argparse import ArgumentParser
+from ipaddress import ip_address
+
+
+class PortScan(Thread):
+    TIMEOUT = 5  # 默认扫描超时时间
+
+    def __init__(self, queue):
+        Thread.__init__(self)
+        self.queue = queue
+
+    def auth(self, url):
+        host = url.split(':')[0]
+        port = url.split(':')[-1]
+
+        try:
+            tn = Telnet(host=host, port=port, timeout=self.TIMEOUT)
+            # 越高,得到的调试输出就越多(sys.stdout)。
+            tn.set_debuglevel(3)
+            print('[*] ' + url + ' -- ok')
+
+            with open('ok.txt', 'a') as f:
+                try:
+                    f.write(str(url) + '\n')
+                except:
+                    pass
+        except Exception as e:
+            # print('[ ] ' + url + ' -- ' + str(e))
+            pass
+        finally:
+            tn.close()
+
+    def run(self):  # 非空
+        while not self.queue.empty():
+            url = self.queue.get()
+            try:
+                self.auth(url)
+            except:
+                continue
+
+
+def dispatcher(url_file=None, ip=None, max_thread=100, portlist=None):
+    iplist = []  # ['207.148.23.27', '47.95.232.119']
+    if url_file is not None and ip is None:
+        with open(str(url_file)) as f:
+            while True:
+                line = str(f.readline()).strip()
+                if line:
+                    iplist.append(line)
+                else:
+                    break
+    elif ip is not None and url_file is None:
+        iplist.append(ip)
+    else:
+        pass
+
+    with open('ok.txt', 'w'):
+        pass
+
+    q = Queue()
+    for ip in iplist:
+        for port in portlist:
+            url = str(ip) + ':' + str(port)
+            q.put(url)
+
+    # print(q.__dict__['queue'])
+    print('-' * 64)
+    print('队列大小：' + str(q.qsize()))
+
+    threadl = [PortScan(q) for _ in range(max_thread)]
+    for t in threadl:
+        t.start()
+
+    for t in threadl:
+        t.join()
+
+
+if __name__ == '__main__':
+    parser = ArgumentParser(add_help=True, description='Port scan tool..')
+    parser.add_argument('-f', dest="url_file", help="Set subdomain file")
+    parser.add_argument('-t', dest="max_threads", nargs='?', type=int, default=1, help="Set max threads")
+    parser.add_argument('--ip', dest='ip', nargs='?', type=str, help="Example: 207.148.23.27, www.ihoneysec.top")
+    parser.add_argument('--port', dest='port', nargs='?', type=str, help="Example: 80    80-89    80,443,3306,8080")
+
+    args = parser.parse_args()
+    logo_code = 'IF9fX18gICAgICAgICAgICBfICAgX19fXyAgICAgICAgICAgICAgICAgIAp8ICBfIFwgX19fICBfIF9ffCB8Xy8gX19ffCAgX19fIF9fIF8gXyBfXyAgCnwgfF8pIC8gXyBcfCAnX198IF9fXF9fXyBcIC8gX18vIF9gIHwgJ18gXCAKfCAgX18vIChfKSB8IHwgIHwgfF8gX19fKSB8IChffCAoX3wgfCB8IHwgfAp8X3wgICBcX19fL3xffCAgIFxfX3xfX19fLyBcX19fXF9fLF98X3wgfF98CiAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAK'
+    print(b64decode(logo_code).decode())
+    print("Example：\npython3.5 portScan.py --ip 192.168.0.105 --port 20-3390 -t 100\npython3.5 portScan.py --ip www.ihoneysec.top --port 80,443,3306,3389,8080 -t 100\npython3.5 xingyuPortScan1.2.py -f ip.txt -t 100")
+
+    print('-' * 64)
+
+    # 端口格式: 80,8080    1-65535    80
+    portlist1 = args.port
+    if portlist1:
+        if ',' in portlist1:
+            portlist = portlist1.split(',')
+        elif '-' in portlist1:
+            portlist = portlist1.split('-')
+            tmpportlist = []
+            for i in range(int(portlist[0]), int(portlist[1]) + 1):
+                tmpportlist.append(i)
+            portlist = tmpportlist
+        else:
+            portlist = [portlist1]
+    else:
+        portlist = [21, 22, 23, 53, 80, 111, 139, 161, 389, 443, 445, 512, 513, 514,
+                    873, 1025, 1433, 1521, 3128, 3306, 3311, 3312, 3389, 5432, 5900,
+                    5984, 6082, 6379, 7001, 7002, 8000, 8080, 8081, 8090, 9000, 9090,
+                    8888, 9200, 9300, 10000, 11211, 27017, 27018, 50000, 50030, 50070]
+    print(
+        'You do not specify a port, the default will scan the following port')
+    if args.ip:  # 指定单个ip扫描方式：指定ip、指定线程
+        try:
+            ip_address(args.ip)
+            dispatcher(ip=args.ip, max_thread=args.max_threads, portlist=portlist)
+        except Exception as e:
+            print(e)
+    elif args.url_file:  # 指定文件扫描方式：指定文件名、线程数
+        dispatcher(url_file=args.url_file, max_thread=args.max_threads, portlist=portlist)
+    else:
+        print(
+            "Please specify the IP address or domain name and the port scanning \nExample：\npython3.5 portScan.py --ip 192.168.0.105 --port 20-3390 \npython3.5 portScan.py --ip www.ihoneysec.top --port 80,443,3306,3389,8080")
